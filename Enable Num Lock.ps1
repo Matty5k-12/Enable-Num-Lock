3dca3a4bc4f57226099cb2ddfb4c2ec215a6d7a8
Matty5k <66275168+Matty5k-12@users.noreply.github.com>
2021-10-27 14:44:51 -0400

    Update Enable Num Lock.ps1

--- Enable Num Lock.ps1
+++ Enable Num Lock.ps1
@@ -1,1 +1,44 @@
-Set-ItemProperty -Path 'Registry::HKU\.DEFAULT\Control Panel\Keyboard' -Name "InitialKeyboardIndicators" -Value "2"
+Param(
+    [Parameter(Mandatory=$false)]
+    [ValidateSet("On", "Off")]
+    [String[]] $onoff='On'
+)
+
+# Get the current state of the Num Lock key
+$CurrentState = [console]::NumberLock
+
+# the RequestedState, based in the command line param.  
+# On is true, Off is false
+if ($onoff -eq 'On') {
+    $RequestedState = $true
+}
+else {
+    $RequestedState = $false
+}
+
+# If the requested state is the current state, we declare
+# victory and go home
+if ($RequestedState -eq $CurrentState) {
+    if ($CurrentState -eq $false) 
+    {
+        Write-Host 'Num Lock is already off'
+    }
+    else {
+        Write-Host 'Num Lock is already on'
+    }
+}
+else {
+    if ($CurrentState -eq $false) 
+    {
+        Write-Host 'Num Lock is off, turning on'
+    }
+    else {
+        Write-Host 'Num lock is on, turning off'
+    }
+    # If the requested state is not the current state, then
+    # we need to do a Num Lock press
+
+    # Create a new instance of the WScript object and send
+    # the NumLock key press to it
+    (New-Object –ComObject WScript.Shell).SendKeys('{NUMLOCK}')
+}
